8eeb8432a095f3cbfac17be52153159c9a048aae
Carlos Herrera <carherco@gmail.com>
2020-05-09 19:22:35 +0200

    actualización donaciones mostrar modificador de precio en la tienda

--- src/app/pages/shop-page/shop-page.component.ts
+++ src/app/pages/shop-page/shop-page.component.ts
@@ -11,7 +11,19 @@ export class ShopPageComponent implements OnInit {
     15, 16, 17, 18, 19, 20, 21,
     107, 23, 113, 81, 88, 130
   ];
-  constructor() { }
+
+  priceModifier = 0;
+
+  reputation = -3;
+  private priceModifierMap = [
+    -5, -5, -4, -4, -4, -4, -3, -3, -3, -3, -2, -2, -2, -2, -1, -1, -1, -1,
+    0, 0, 0, 0, 0,
+    1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5
+  ];
+
+  constructor() {
+    this.priceModifier = this.priceModifierMap[20 - this.reputation];
+  }
 
   ngOnInit(): void {
   }
