5a39d1b75b62097457e46e8fba004e0e0c1711d3
Carlos Herrera <carherco@gmail.com>
2020-05-11 12:40:46 +0200

    poder girar objetos

--- src/app/components/items-tracker/items-tracker.component.ts
+++ src/app/components/items-tracker/items-tracker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Item } from 'src/app/model/item';
 
 @Component({
   selector: 'app-items-tracker',
@@ -7,10 +8,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemsTrackerComponent implements OnInit {
 
-  @Input() items;
+  @Input() itemNumbers: number[] = [];
+  items: Item[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = this.itemNumbers.map(
+      id => { return {id, spent: false, consumed: false, flipped: false}; }
+    );
+  }
+
+  toggleSpent(item: Item) {
+    item.spent = !item.spent;
+  }
+
+  toggleConsumed(item: Item) {
+    item.consumed = !item.consumed;
+  }
+
+  toggleFlipped(item: Item) {
+    item.flipped = !item.flipped;
   }
 
 }
---
+++ src/app/model/item.ts
@@ -0,0 +1,8 @@
+export interface Item {
+  id: number;
+  name?: string;
+  image?: string;
+  spent?: boolean;
+  consumed?: boolean;
+  flipped?: boolean;
+}
