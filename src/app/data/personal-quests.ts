3a0ece5c7cd18d79ac1507e38c524396a9b7c982
Carlos Herrera <carherco@gmail.com>
2022-09-02 12:41:48 +0200

    datos de personal quests

--- src/app/data/personal-quests.ts
+++ src/app/data/personal-quests.ts
@@ -5,7 +5,28 @@ export interface PersonalQuestDef {
 }
 
 export const PERSONAL_QUESTS = {
-  513: { id: 513, title: 'Augmented Abilities', total: 4 },
-  528: { id: 528, title: 'Augmented Abilities', total: 4 },
-  530: { id: 530, title: 'Augmented Abilities', total: 4 },
-}
+  510: {id: 510, title: 'Seeker of Xorn', total: 3 },
+  511: {id: 511, title: 'Merchant Class', total: 13 },
+  512: {id: 512, title: 'Greed is Good', total: 200 },
+  513: {id: 513, title: 'Finding the Cure', total: 8 },
+  514: {id: 514, title: 'A Study of Anatomy', total: 15 },
+  515: {id: 515, title: 'Law Bringer', total: 20 },
+  516: {id: 516, title: 'Pounds of Flesh', total: 15 },
+  517: {id: 517, title: 'Trophy Hunt', total: 20 },
+  518: {id: 518, title: 'Eternal Wanderer', total: 15 },
+  519: {id: 519, title: 'Battle Legend', total: 15 },
+  520: {id: 520, title: 'Implement of Light', total: 7 },
+  521: {id: 521, title: 'Take Back the Trees', total: 3 },
+  522: {id: 522, title: 'The Thin Places', total: 6 },
+  523: {id: 523, title: 'Aberrant Slayer', total: 6 },
+  524: {id: 524, title: 'Fearless Stand', total: 20 },
+  525: {id: 525, title: 'Piety in all Things', total: 12 },
+  526: {id: 526, title: 'Vengeance', total: 4 },
+  527: {id: 527, title: 'Zealot of the Blood God', total: 12 },
+  528: {id: 528, title: 'Goliath Toppler', total: 4 },
+  529: {id: 529, title: 'The Fall of Man', total: 2 },
+  530: {id: 530, title: 'Augmented Abilities', total: 4 },
+  531: {id: 531, title: 'Elemental Samples', total: 6 },
+  532: {id: 532, title: 'A Helping Hand', total: 2 },
+  533: {id: 533, title: 'The Perfect Poison', total: 9 },
+};
