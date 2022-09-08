6050896875e7202b35dfec9f6ce980f26ba17f0b
Carlos Herrera <carherco@gmail.com>
2022-09-08 16:03:29 +0200

    sustituyo ítems de la tienda por los calculados automáticamente

--- src/app/pages/shop-page/shop-page.component.ts
+++ src/app/pages/shop-page/shop-page.component.ts
@@ -10,16 +10,7 @@ import { CampaignStatusService } from 'src/app/services/campaign-status.service'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShopPageComponent {
-  itemIndexes = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28,
-    29, 30, 31, 32, 33, 34, 35,
-    36, 37, 38, 39, 40, 41, 42,
-    125, 107, 113, 81, 88, 130, 116, 74, 44, 98, 77,
-    135, 139, 147, 76, 108, 112, 103, 123,
-    75, 133
-  ];
+  itemIndexes = [];
 
   items: ItemDef[] = [];
   itemTypeSelected: ItemType = 'all';
@@ -37,7 +28,5 @@ export class ShopPageComponent {
         this.items = ITEMS.filter( (_item, index) => this.itemIndexes.includes(index + 1) );
       }
     );
-
-
   }
 }
